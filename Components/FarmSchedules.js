543f4456cc3506dbfe1b2ea01083c4eaebfbd9e9
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-10-28 18:59:10 +0100

    turned farm schedules into table

--- Components/FarmSchedules.js
+++ Components/FarmSchedules.js
@@ -8,31 +8,50 @@ import {
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Table, Row } from 'react-native-table-component';
 
 export default class EditFarmer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      tableHead: ['Jour', 'Horaires', 'Activité'],
+      widthArr: [100, 120, 120]
+    }
   };
 
   render() {
     const { route, navigation } = this.props;
     const { item } = route.params;
     const { FarmSchedules } = route.params.item;
-    console.log(FarmSchedules);
+    const tableRow = FarmSchedules.map(item=>([item.day, item.start_time + ' à ' + item.end_time, item.activity]));
+    // console.log(FarmSchedules);
+    // console.log(tableRow);
 
     return (
-      <View style={styles.MainContainer}>
-        <ScrollView>
-          {
-            FarmSchedules.map((item) => (
-              <React.Fragment key={item.id.toString()}>
-                <Text style={styles.DataStyle}>Jour : {item.day}</Text>
-                <Text style={styles.DataStyle}>Activité: {item.activity}</Text>
-                <Text style={styles.DataStyle}>Horaires : de {item.start_time} à {item.end_time}</Text>
-              </React.Fragment>
-            ))
-          }
+      <View style={styles1.container}>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row data={this.state.tableHead} widthArr={this.state.widthArr} style={{height: 50}} textStyle={{textAlign: 'center', fontWeight: '100'}}/>
+            </Table>
+            <ScrollView style={styles1.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              {
+                tableRow.map((rowData, index) => (
+                  <Row
+                    key={index}
+                    data={rowData}
+                    widthArr={this.state.widthArr}
+                    style={[styles1.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                    textStyle={styles1.text}
+                  />
+                ))
+              }
+              </Table>
+            </ScrollView>
+          </View>
         </ScrollView>
+
         {/* back to farm details */}
         <TouchableOpacity
           style={{ backgroundColor: '#FF0099'}}
@@ -93,4 +112,12 @@ const styles = StyleSheet.create({
     textAlign:'center',
     textTransform: 'uppercase'
   }
+});
+
+const styles1 = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
 });
