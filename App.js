2860436974214884fd065faa9fb9a2af8945ccf4
Ali Raza Syed <a.syed@coceur.online>
2020-10-28 14:36:23 +0100

    tried to integrate drawerMenu

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Navigations from './Navigations/Navigations';
-import MyDrawer from './Navigations/DrawerNavigation';
+// import MyDrawer from './Navigations/DrawerNavigation';
 // import { Provider } from 'react-redux';
 
 export default class App extends React.Component {
