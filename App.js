efb8de6ebf18db0d058694fb8b296ef2257683fc
cyla00 <ikhayam000@protonmail.com>
2020-10-28 16:36:25 +0100

    general search

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Search from './Components/search.js';
+import GeneralSearch from './Components/generalSearch.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Search/>
+      <GeneralSearch/>
     </View>
   );
 }
--- Components/generalSearch.js
+++ Components/generalSearch.js
@@ -2,56 +2,37 @@ import React from 'react';
 import { StyleSheet, View, Button, TextInput, FlatList, Text, Image, ActivityIndicator, ImageBackground, TouchableOpacity, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default class Search extends React.Component {
+export default class GeneralSearch extends React.Component {
     constructor(props){
       super(props)
       this.state = {
-         food: [],
-        nutrients: [],
-        defaultImg: null,
-        FoodLabel: "",
-        searchText: ""
+         searchedText: "",
+
         }
     }
   
-  
-  _getFood(){
-    const API_ID = '24e1badd';
-    const API_TOKEN = "5415e30a26af09724f832adf15f972f9";
-    const url='https://api.edamam.com/api/food-database/v2/parser?ingr='+ this.state.searchText +'&app_id='+API_ID+'&app_key='+API_TOKEN+'';
-  
-    fetch(url)
-    .then(elements => elements.json().then(data => {
-      const foods = data.hints[0].food;
-      const nutrients = foods.nutrients;
-      this.setState({food: foods, nutrients: nutrients});
-  
-      const label =  this.state.food.label;
-      const upper = label.toUpperCase();
-      this.setState({FoodLabel: upper});
-  
-  
-      if(typeof this.state.food.image == 'undefined'){
-        const defImg = "https://i.ibb.co/sqtkkkr/diet.png";
-        this.setState({defaultImg: defImg});
-      }
-      else{
-        this.setState({defaultImg: this.state.food.image});
-      }
-    }))
-    .catch(err => console.log(err))
-  }
-  
-  
-  _loadAliments() {
-      this._getFood(this.state.searchText);
-  }
-  
-  _searchText(txt){
-    this.state.searchText = txt;
-  }
-  
-  
+    _getFarms(){
+        const url = "https://local-farmers-api.herokuapp.com/API/search/farms?value="+  this.state.searchedText +"";
+        fetch(url).then(fetched => fetched.json().then(result => {
+            result.forEach(element => {
+                const elm = element;
+                console.log(elm.address);
+            });
+        }))
+        .catch(err => console.log(err))
+
+        console.log(this.state.searchedText);
+    }
+
+    _loadData() {
+        this._getFarms(this.state.searchText);
+    }
+
+    _contentTxt(ctx){
+        this.state.searchedText = txt;
+    }
+
+ 
     render() {
       return(
         <SafeAreaView style={stileGeneral.Wrapper}>
@@ -62,13 +43,14 @@ export default class Search extends React.Component {
             </TouchableOpacity>
 
             <Image style={stileGeneral.imgIcon} source={{ uri: 'https://i.ibb.co/d0TCxN9/shop-1.png' }}/>
-            <Text style={stileGeneral.Title}>CARTE</Text>
+            <Text style={stileGeneral.Title}>APP NAME</Text>
           </View>
 
           <View>
             <View style={stileGeneral.searchWrapper}>
-              <TextInput style={stileGeneral.ricerca} onChangeText={(txt) => this._searchText(txt)} placeholder="cerca" onSubmitEditing={} />
+              <TextInput style={stileGeneral.ricerca} onChangeText={(txt) => this._contentTxt(txt)} placeholder="cerca" onSubmitEditing={() => this._loadData()} onPress={this._getFarms()}/>
             </View>
+
           </View>
           
         </SafeAreaView>
@@ -78,5 +60,9 @@ export default class Search extends React.Component {
   
   const stileGeneral = StyleSheet.create({
     // #######
-
+    Wrapper: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#08585E',
+     },
   })
